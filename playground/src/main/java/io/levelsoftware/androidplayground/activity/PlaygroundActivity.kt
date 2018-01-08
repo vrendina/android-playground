b95863014a9409bc1bebc8e7624d88045a2152bb
Victor Rendina <vrendina@gmail.com>
2018-01-08 07:54:10 -0500

    Support for animated hiding tabs in progress

--- playground/src/main/java/io/levelsoftware/androidplayground/activity/PlaygroundActivity.kt
+++ playground/src/main/java/io/levelsoftware/androidplayground/activity/PlaygroundActivity.kt
@@ -1,15 +1,24 @@
 package io.levelsoftware.androidplayground.activity
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.ValueAnimator
 import android.annotation.SuppressLint
+import android.os.Build.VERSION
+import android.os.Build.VERSION_CODES
 import android.os.Bundle
 import android.support.design.internal.BottomNavigationItemView
 import android.support.design.internal.BottomNavigationMenuView
 import android.support.design.widget.BottomNavigationView
 import android.support.design.widget.BottomNavigationView.OnNavigationItemSelectedListener
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.Toolbar
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import kotlinx.android.synthetic.main.activity_playground.appBarLayout
+import kotlinx.android.synthetic.main.activity_playground.bottomNavigation
+import kotlinx.android.synthetic.main.activity_playground.tabLayout
+import kotlinx.android.synthetic.main.activity_playground.toolbar
 import levelsoftware.io.androidplayground.R
 import levelsoftware.io.androidplayground.R.id
 import levelsoftware.io.androidplayground.R.layout
@@ -19,9 +28,6 @@ import java.lang.Exception
 
 
 class PlaygroundActivity : AppCompatActivity(), OnNavigationItemSelectedListener {
-
-  private val toolbar by lazy { findViewById<Toolbar>(id.toolbar) }
-  private val bottomNavigation by lazy { findViewById<BottomNavigationView>(id.bottom_navigation) }
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -42,17 +48,65 @@ class PlaygroundActivity : AppCompatActivity(), OnNavigationItemSelectedListener
 
   override fun onNavigationItemSelected(item: MenuItem): Boolean {
     Timber.d("Selected menu item ${item.title}")
+    when (item.itemId) {
+      R.id.action_android -> hideTabs()
+      R.id.action_beach -> showTabs()
+    }
     return true
   }
 
   override fun onOptionsItemSelected(item: MenuItem): Boolean {
     when (item.itemId) {
-      id.action -> {
-        Timber.d("Clicked on action")
+      id.show -> {
+        showTabs()
+        return true
+      }
+      id.hide -> {
+        hideTabs()
         return true
       }
     }
     return super.onOptionsItemSelected(item)
+  }
+
+  private fun showTabs() {
+//    Timber.d("Toolbar height ${toolbar.height} tablayout height ${tabLayout.height}")
+//    tabLayout.translationY = 0f
+//    appBarLayout.layoutParams.apply {
+//      height = toolbar.height + tabLayout.height
+//      Timber.d("Setting appbar height to: $height")
+////      appBarLayout.layoutParams = this
+//    }
+
+    tabLayout.visibility = View.VISIBLE
+  }
+
+  private fun hideTabs() {
+    val initialHeight = appBarLayout.height
+
+    val animator = ValueAnimator().apply {
+      setIntValues(initialHeight, toolbar.height)
+      duration = 2000L
+      addUpdateListener { animation ->
+        appBarLayout.layoutParams.apply {
+          height = animation.animatedValue as Int
+          tabLayout.translationY = (height - initialHeight).toFloat()
+          Timber.d(
+              "Setting appbar height to: $height translation to ${tabLayout.translationY} y value ${tabLayout.y} height ${tabLayout.height}")
+          appBarLayout.layoutParams = this
+        }
+
+      }
+
+      addListener(object : AnimatorListenerAdapter() {
+        override fun onAnimationEnd(animation: Animator) {
+          if (VERSION.SDK_INT >= VERSION_CODES.LOLLIPOP) {
+            appBarLayout.elevation = 4f * resources.displayMetrics.density
+          }
+        }
+      })
+    }
+    animator.start()
   }
 
 
